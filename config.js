dcd3f9658306626c99fe4d8c0006f5793f610756
Revalto <samuel.nestro@mail.ru>
2019-07-16 03:39:47 +0300

    relese

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const groupToken 	= 'ba26ab4a132b3a130944165bd7a0508fa624e4dac8b429ed77c4383226b5c6d7bfcd6bc997e33f703a2dd';
-const groupID 	 	= 167247627;
+const groupToken 	= 'Ваш токен';
+const groupID 	 	= ид страницы;
 
 module.exports = {
   groupToken,
